8319cf5269e345e3cf04de2b4e7365d52c793a29
jdecroock <decroockjovi@gmail.com>
2020-01-04 00:10:14 +0100

    add some basic modules

---
+++ src/modules/auth/index.tsx
@@ -0,0 +1,4 @@
+import { h } from 'preact';
+
+// TODO: construct common Auth route!
+export default () => <p>Auth</p>
---
+++ src/modules/landing/index.tsx
@@ -0,0 +1,11 @@
+import { h } from 'preact';
+import { TextField, Button } from '../../common';
+
+// TODO: construct common Landing route!
+export default () => (
+  <div>
+    <Button />
+    <TextField />
+    <p>Landing</p>
+  </div>
+)
