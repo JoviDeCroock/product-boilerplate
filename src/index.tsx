6c706890a8e6db4c55429781fd9001097f8e8eb6
Jovi De Croock <decroockjovi@gmail.com>
2020-02-19 00:58:30 +0100

    specific logs

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,8 @@ import { App } from './App';
 setPragma(h);
 normalize();
 
-console.log(process.env);
+console.log(process.env.API_URL);
+console.log(process.env.DEPLOY_URL);
 
 const root = document.getElementById('root');
 if (root) {
